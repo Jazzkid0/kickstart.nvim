ffdbdbd9122a5bf833e99f0639ba57856fd1f7e8
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2024-03-04 08:58:13 +0000

    file-browser

--- lua/custom/plugins/file-browser.lua
+++ lua/custom/plugins/file-browser.lua
@@ -1,25 +1,9 @@
 return {
   'nvim-telescope/telescope-file-browser.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+  vim.api.nvim_set_keymap('n', '<space>fb', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', { noremap = true }),
 
-  config = function()
-    local telescopefs = require 'telescope-file-browser'
-    telescopefs.setup {
-      extensions = {
-        file_browser = {
-          theme = 'ivy',
-          -- disables netrw and use telescope-file-browser in its place
-          hijack_netrw = true,
-          mappings = {
-            ['i'] = {
-              -- your custom insert mode mappings
-            },
-            ['n'] = {
-              -- your custom normal mode mappings
-            },
-          },
-        },
-      },
-    }
+  setup = function()
+    require('telescope').load_extension 'file_browser'
   end,
 }
