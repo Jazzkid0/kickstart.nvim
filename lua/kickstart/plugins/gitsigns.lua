c0482af8c9f8abe5f113736b9cfb1c5bd74f0667
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2024-03-04 02:50:04 +0000

    harpoon

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -2,16 +2,14 @@
 -- See `:help gitsigns` to understand what the configuration keys do
 
 return {
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
     },
   },
 }
