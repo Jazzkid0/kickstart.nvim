327653972bffc8e27b68440ce3a55cf5bc4ea675
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2024-04-03 04:21:18 +0100

    finally fixed colours

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -4,13 +4,19 @@ return { -- You can easily change to a different colorscheme.
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    -- Load the colorscheme here
-    vim.cmd.colorscheme 'tokyonight-night'
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'None' })
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'None' })
-    vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#111113' })
+    local tokyonight = require 'tokyonight'
 
-    -- You can configure highlights by doing something like
-    vim.cmd.hi 'Comment gui=none'
+    tokyonight.setup({
+      on_colors = function(colors)
+        colors.bg = '#000000'
+      end,
+
+      on_highlights = function(hl)
+        hl.CursorLine = { fg = nil, bg = "#111113" }
+      end,
+    })
+
+    vim.cmd.colorscheme 'tokyonight-night'
   end,
+
 }
