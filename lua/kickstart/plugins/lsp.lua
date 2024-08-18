7be6d3ee5afa1ca9a27484bb28a703bbbd44f38f
Jazzkid0 <28173360+Jazzkid0@users.noreply.github.com>
2024-08-18 19:25:02 +0100

    rm gopls

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -94,7 +94,6 @@ return { -- LSP Configuration & Plugins
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
-      gopls = {},
       rust_analyzer = {
         ["rust_analyzer"] = {
           checkOnSave = {
